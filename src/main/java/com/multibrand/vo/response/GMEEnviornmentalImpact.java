04f4e59e9ae2bef8149b83c7eab6affccadc8f52
NGASPerera <NGASPerera@TXHQNGASPERE-L1.nrgenergy.com>
2019-03-21 11:38:33 -0500

    Added environmental impact API to GME mobile app.

---
+++ src/main/java/com/multibrand/vo/response/GMEEnviornmentalImpact.java
@@ -0,0 +1,24 @@
+package com.multibrand.vo.response;
+
+public class GMEEnviornmentalImpact {
+
+	private double totalPoundOfCO2ForCurrentYr;
+	private double toatlTreesPlantedForCurrentYr;
+	
+	public double getTotalPoundOfCO2ForCurrentYr() {
+		return totalPoundOfCO2ForCurrentYr;
+	}
+	public void setTotalPoundOfCO2ForCurrentYr(double totalPoundOfCO2ForCurrentYr) {
+		this.totalPoundOfCO2ForCurrentYr = totalPoundOfCO2ForCurrentYr;
+	}
+	public double getToatlTreesPlantedForCurrentYr() {
+		return toatlTreesPlantedForCurrentYr;
+	}
+	public void setToatlTreesPlantedForCurrentYr(double toatlTreesPlantedForCurrentYr) {
+		this.toatlTreesPlantedForCurrentYr = toatlTreesPlantedForCurrentYr;
+	}
+	
+	
+
+	
+}
