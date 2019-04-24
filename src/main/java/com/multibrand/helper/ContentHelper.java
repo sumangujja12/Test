9162dc3238f060126403077e44a44d6f2c91ee9d
SMarimuthu <SMarimuthu@TXHQSMARIMUT-L1.nrgenergy.com>
2019-04-24 14:30:45 -0500

    Committed for Bug Fixed

--- src/main/java/com/multibrand/helper/ContentHelper.java
+++ src/main/java/com/multibrand/helper/ContentHelper.java
@@ -14,7 +14,6 @@ import java.util.regex.Pattern;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
