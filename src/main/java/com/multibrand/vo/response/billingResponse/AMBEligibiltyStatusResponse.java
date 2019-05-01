61fb08af0508b8e2c639f1b2c53eb13aaec7232a
NGASPerera <NGASPerera@TXHQNGASPERE-L1.nrgenergy.com>
2019-05-01 13:42:44 -0500

    Added code refactoring for ambEligibility check for GME mobile app.

---
+++ src/main/java/com/multibrand/vo/response/billingResponse/AMBEligibiltyStatusResponse.java
@@ -0,0 +1,21 @@
+package com.multibrand.vo.response.billingResponse;
+
+public class AMBEligibiltyStatusResponse{
+
+	private String avgBillFlag;
+	private String avlBillFlag;
+	
+	public String getAvgBillFlag() {
+		return avgBillFlag;
+	}
+	public void setAvgBillFlag(String avgBillFlag) {
+		this.avgBillFlag = avgBillFlag;
+	}
+	public String getAvlBillFlag() {
+		return avlBillFlag;
+	}
+	public void setAvlBillFlag(String avlBillFlag) {
+		this.avlBillFlag = avlBillFlag;
+	}
+	
+}
