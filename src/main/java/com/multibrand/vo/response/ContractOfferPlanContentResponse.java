dfea33d56f5285a1a8777f8263722ea53a65186f
SMarimuthu <SMarimuthu@TXHQSMARIMUT-L1.nrgenergy.com>
2019-05-02 09:54:50 -0500

    Remove unwanted Code

--- src/main/java/com/multibrand/vo/response/ContractOfferPlanContentResponse.java
+++ src/main/java/com/multibrand/vo/response/ContractOfferPlanContentResponse.java
@@ -13,11 +13,6 @@ public class ContractOfferPlanContentResponse extends GenericResponse {
 	private ContractOffer currentPlan;
 	private List<ContractOffer> plans;
 	private ServiceAddressDO serviceAddress;
-	private boolean renewalOffers = false;
-	private boolean swapOffers = false;
-	private String swapPendingDate ="";
-	private boolean pendingSwap = false;
-	private String yearlyTreesAbsorbed = "";
 
 	/**
 	 * @return the plans
@@ -60,75 +55,6 @@ public class ContractOfferPlanContentResponse extends GenericResponse {
 	public void setServiceAddress(ServiceAddressDO serviceAddress) {
 		this.serviceAddress = serviceAddress;
 	}
-
-	/**
-	 * @return the renewalOffers
-	 */
-	public boolean isRenewalOffers() {
-		return renewalOffers;
-	}
-
-	/**
-	 * @param renewalOffers the renewalOffers to set
-	 */
-	public void setRenewalOffers(boolean renewalOffers) {
-		this.renewalOffers = renewalOffers;
-	}
-
-	/**
-	 * @return the swapOffers
-	 */
-	public boolean isSwapOffers() {
-		return swapOffers;
-	}
-
-	/**
-	 * @param swapOffers the swapOffers to set
-	 */
-	public void setSwapOffers(boolean swapOffers) {
-		this.swapOffers = swapOffers;
-	}
-
-	/**
-	 * @return the swapPendingDate
-	 */
-	public String getSwapPendingDate() {
-		return swapPendingDate;
-	}
-
-	/**
-	 * @param swapPendingDate the swapPendingDate to set
-	 */
-	public void setSwapPendingDate(String swapPendingDate) {
-		this.swapPendingDate = swapPendingDate;
-	}
-
-	/**
-	 * @return the pendingSwap
-	 */
-	public boolean isPendingSwap() {
-		return pendingSwap;
-	}
-
-	/**
-	 * @param pendingSwap the pendingSwap to set
-	 */
-	public void setPendingSwap(boolean pendingSwap) {
-		this.pendingSwap = pendingSwap;
-	}
-
-	/**
-	 * @return the yearlyTreesAbsorbed
-	 */
-	public String getYearlyTreesAbsorbed() {
-		return yearlyTreesAbsorbed;
-	}
-
-	/**
-	 * @param yearlyTreesAbsorbed the yearlyTreesAbsorbed to set
-	 */
-	public void setYearlyTreesAbsorbed(String yearlyTreesAbsorbed) {
-		this.yearlyTreesAbsorbed = yearlyTreesAbsorbed;
-	}
+	
 	
 }
