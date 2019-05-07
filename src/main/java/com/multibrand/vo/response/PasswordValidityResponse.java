137d9452f85e4d6763bbbd65a548157308d77154
NGASPerera <NGASPerera@TXHQNGASPERE-L1.nrgenergy.com>
2019-05-07 12:03:54 -0500

    Added validatePassword Api for GME Mobile App.

---
+++ src/main/java/com/multibrand/vo/response/PasswordValidityResponse.java
@@ -0,0 +1,10 @@
+package com.multibrand.vo.response;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import org.springframework.stereotype.Component;
+
+@XmlRootElement(name="validPassword")
+@Component
+public class PasswordValidityResponse extends GenericResponse{
+
+}
