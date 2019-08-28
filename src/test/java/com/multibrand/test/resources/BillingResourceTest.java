6c485bff9c717615e3d63c6c86680fac5877ea82
NGASPerera <NGASPerera@TXHQNGASPERE-L1.nrgenergy.com>
2019-08-28 11:21:14 -0500

    Added unit test cases for getBillingAddress Api.

--- src/test/java/com/multibrand/test/resources/BillingResourceTest.java
+++ src/test/java/com/multibrand/test/resources/BillingResourceTest.java
@@ -1,4 +1,4 @@
-package com.multibrand.resources.test;
+package com.multibrand.test.resources;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -6,12 +6,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
@@ -21,65 +20,45 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import com.multibrand.helper.AsyncHelper;
+import com.multibrand.bo.BillingBO;
+import com.multibrand.bo.RegistrationBO;
 import com.multibrand.helper.UtilityLoggerHelper;
 import com.multibrand.resources.BillingResource;
-import com.multibrand.service.ProfileService;
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 @TestPropertySource({"file:src/test/resources/properties/environment.properties","file:src/test/resources/properties/appConstants.properties"})
 @ContextConfiguration({"file:src/test/resources/webapp/WEB-INF/spring/NRGREST-appContext.xml"})
+
 public class BillingResourceTest {
-	
-	@Value("${isMock}")
-	private boolean isMock;
+
 	
 	private MockMvc mockMvc;
 	
 	@InjectMocks
 	private BillingResource billingResource;
 	
-	@InjectMocks
-	ProfileService profileService;
+	@Mock
+	RegistrationBO registrationBO;
 	
+	@MockBean
+	BillingBO billingBo;
 	
 	@MockBean
 	private UtilityLoggerHelper utilityloggerHelper;
 	
-	
 	@Autowired
 	private WebApplicationContext wac;
 	
-	@Autowired
-	  @Qualifier("appConstMessageSource")
-	  public ReloadableResourceBundleMessageSource appConstMessageSource;
-	
-	 
-	 
-
-	 
-	
-	private AsyncHelper asycHelper = new AsyncHelper();
 	
 	@Before
 	public void init() {
-		 MockitoAnnotations.initMocks(this); 
-		 if (isMock) { 
-			  MockitoAnnotations.initMocks(this); 
-			  mockMvc = MockMvcBuilders.standaloneSetup().build(); 
-		 } else { 
-			 mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build(); 
-		} 
+		mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build(); 
 	}
 	
 	
 	@Test
 	public void test_getBillingAddress_success() throws Exception {
-		if(isMock) {
-			System.out.println("isMock :::::   ********************************************************************************************************** " +isMock);
-		} else {
 			mockMvc.perform( MockMvcRequestBuilders
 					  .post("/billResource/getBillingAddress")
 					  .param("accountNumber","000072938063") 
@@ -92,8 +71,9 @@ public class BillingResourceTest {
 		 
 		 
 		 
-		 
-	}
+
+	
+	
 }
 
 
