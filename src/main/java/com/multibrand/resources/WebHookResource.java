8d48dc18394037b8b4c015e05ef7f514cfd54e9b
vanagani <VAnagani@nrgenergy.com>
2020-01-06 15:42:06 -0600

    VD web hook code cleanup

--- src/main/java/com/multibrand/resources/WebHookResource.java
+++ src/main/java/com/multibrand/resources/WebHookResource.java
@@ -1,11 +1,9 @@
 package com.multibrand.resources;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
