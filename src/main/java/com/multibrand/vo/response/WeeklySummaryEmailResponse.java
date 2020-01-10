f3cd98543a1dc85903cd9f7e4b539672a5451bb0
Dasari, Murali <Murali.Dasari@nrg.com>
2020-01-10 15:26:20 -0600

    Removed error code and error description from the pojo and reused those are coming from the generic response pojo

--- src/main/java/com/multibrand/vo/response/WeeklySummaryEmailResponse.java
+++ src/main/java/com/multibrand/vo/response/WeeklySummaryEmailResponse.java
@@ -8,10 +8,9 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-@XmlRootElement(name="WeeklySummaryEmailResponse")
-@XmlSeeAlso({WseDO.class,WseResponse.class})
-public class WeeklySummaryEmailResponse extends GenericResponse
-{
+@XmlRootElement(name = "WeeklySummaryEmailResponse")
+@XmlSeeAlso({ WseDO.class, WseResponse.class })
+public class WeeklySummaryEmailResponse extends GenericResponse {
 	@SerializedName("contract_account_number")
 	@Expose
 	private String contractAccountNumber;
@@ -21,42 +20,28 @@ public class WeeklySummaryEmailResponse extends GenericResponse
 	@SerializedName("wseList")
 	@Expose
 	private List<WseResponse> wseList = null;
-	@SerializedName("error_code")
-	@Expose
-	private String errorCode;
-	@SerializedName("error_message")
-	@Expose
-	private String errorMessage;
-	
-	
+
 	public String getContractAccountNumber() {
 		return contractAccountNumber;
 	}
+
 	public void setContractAccountNumber(String contractAccountNumber) {
 		this.contractAccountNumber = contractAccountNumber;
 	}
+
 	public String getCompanyCode() {
 		return companyCode;
 	}
+
 	public void setCompanyCode(String companyCode) {
 		this.companyCode = companyCode;
 	}
+
 	public List<WseResponse> getWseList() {
 		return wseList;
 	}
+
 	public void setWseList(List<WseResponse> wseList) {
 		this.wseList = wseList;
-	}
-	public String getErrorCode() {
-		return errorCode;
-	}
-	public void setErrorCode(String errorCode) {
-		this.errorCode = errorCode;
-	}
-	public String getErrorMessage() {
-		return errorMessage;
-	}
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
 	}
 }
