0d0065e3fc86326c6130945c8040b56fed78ea89
Dasari, Murali <Murali.Dasari@nrg.com>
2020-01-13 13:46:16 -0600

    Changed http method from GET to POST for WSE rest service

--- src/main/java/com/multibrand/resources/WeeklySummaryEmailResource.java
+++ src/main/java/com/multibrand/resources/WeeklySummaryEmailResource.java
@@ -2,7 +2,7 @@ package com.multibrand.resources;
 
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -35,7 +35,7 @@ public class WeeklySummaryEmailResource  extends BaseResource {
 	 * @param WeeklySummaryEmailRequest
 	 * @return
 	 */
-	@GET
+	@POST
 	@Path("/historicalWse")
 	@Consumes({ MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_JSON})
