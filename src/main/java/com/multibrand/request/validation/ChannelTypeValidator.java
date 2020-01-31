a21dbb8581be63597643701433b1e2318f392c75
Arumugam Thirugnanam <AThirug1@retail.nrgenergy.com>
2020-01-31 11:02:56 -0600

    Reliant Sales - Sonar Fix

--- src/main/java/com/multibrand/request/validation/ChannelTypeValidator.java
+++ src/main/java/com/multibrand/request/validation/ChannelTypeValidator.java
@@ -18,7 +18,7 @@ public class ChannelTypeValidator implements
 			ConstraintValidator<ChannelType, String>, Constants{
 	
     private List<String> channelTypeList = null;
-    public static final String[] channelTypeArray = {"WEB","AA","AFF","CC","SMB"}; 
+    private static final String[] channelTypeArray = {"WEB","AA","AFF","CC","SMB"}; 
     
     @Override
 	public boolean isValid(String channelType, ConstraintValidatorContext arg1) {
