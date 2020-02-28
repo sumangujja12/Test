d4ae767da48a19eaac479f9bfc08f2cd02537002
Arumugam Thirugnanam <AThirug1@retail.nrgenergy.com>
2020-02-28 13:39:48 -0600

    Reliant Sales - toString method added.

--- src/main/java/com/multibrand/dto/response/EsidResponse.java
+++ src/main/java/com/multibrand/dto/response/EsidResponse.java
@@ -3,6 +3,9 @@ package com.multibrand.dto.response;
 import java.io.Serializable;
 import java.util.List;
 
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
 import com.multibrand.vo.response.GenericResponse;
 import com.multibrand.vo.request.ESIDData;
 
@@ -20,5 +23,9 @@ public class EsidResponse extends GenericResponse implements Serializable {
 		this.esidList = esidList;
 	}
 
+	public String toString(){
+		return ReflectionToStringBuilder.toString(this,
+				ToStringStyle.SHORT_PREFIX_STYLE);
+	}
 
 }
