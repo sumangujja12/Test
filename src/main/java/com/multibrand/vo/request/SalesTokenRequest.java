8e5259d175b466ec15dd4e81a48cff4a01796d33
VSood30 <VSood30@TXHQVSOOD-L2.nrgenergy.com>
2020-03-19 15:31:07 -0500

    Reliant Sales - Code refactoring for Sales APIs

---
+++ src/main/java/com/multibrand/vo/request/SalesTokenRequest.java
@@ -0,0 +1,47 @@
+package com.multibrand.vo.request;
+
+import javax.ws.rs.QueryParam;
+
+import org.hibernate.validator.constraints.NotBlank;
+import com.multibrand.dto.request.SalesBaseRequest;
+
+public class SalesTokenRequest extends SalesBaseRequest {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 747886355739716064L;
+	
+	@QueryParam(value = "actionCode")
+	@NotBlank
+	private String actionCode;
+	
+	@QueryParam(value = "numToBeTokenized")
+	@NotBlank
+	private String numToBeTokenized;
+	
+	/**
+	 * @return the actionCode
+	 */
+	public String getActionCode() {
+		return actionCode;
+	}
+	/**
+	 * @param actionCode the actionCode to set
+	 */
+	public void setActionCode(String actionCode) {
+		this.actionCode = actionCode;
+	}
+	/**
+	 * @return the numToBeTokenized
+	 */
+	public String getNumToBeTokenized() {
+		return numToBeTokenized;
+	}
+	/**
+	 * @param numToBeTokenized the numToBeTokenized to set
+	 */
+	public void setNumToBeTokenized(String numToBeTokenized) {
+		this.numToBeTokenized = numToBeTokenized;
+	}
+}
