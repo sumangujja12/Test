12384691028dd46e295c6da4801a7e449b090d34
289347 <289347@infarszb11774.lntinfotech.com>
2020-03-23 18:50:18 +0530

    OE :Sprint7 : PBI :23678 & 22623 : ActionCode and DOB Validator

---
+++ src/test/java/com/multibrand/request/validation/AgeValidatorTest.java
@@ -0,0 +1,31 @@
+package com.multibrand.request.validation;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+import org.hibernate.validator.internal.util.annotationfactory.AnnotationDescriptor;
+import org.hibernate.validator.internal.util.annotationfactory.AnnotationFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AgeValidatorTest {
+	
+	private AgeValidator validator;
+	
+	@BeforeMethod
+	public void setup(){
+		AnnotationDescriptor<ValidAge> descriptor = new AnnotationDescriptor<ValidAge>(ValidAge.class);
+		ValidAge validAge = AnnotationFactory.create(descriptor);
+		this.validator = new AgeValidator();
+		this.validator.initialize(validAge);
+	}
+	
+	@Test
+	public void testValidAge() {
+		assertTrue( validator.isValid("04/12/1956", null));
+		
+	}
+	@Test
+	public void testInValidAge() {
+		assertFalse(validator.isValid( "04/12/2003", null));
+	}
+}
