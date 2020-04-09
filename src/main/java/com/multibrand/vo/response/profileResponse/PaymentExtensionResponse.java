250a8ce73f1e672d77f72e3d3841859b4f56985a
SMarimuthu <SMarimuthu@TXHQSMARIMUT-L1.nrgenergy.com>
2020-04-09 14:23:12 -0500

    commited for VO

---
+++ src/main/java/com/multibrand/vo/response/profileResponse/PaymentExtensionResponse.java
@@ -0,0 +1,26 @@
+package com.multibrand.vo.response.profileResponse;
+
+import com.multibrand.vo.response.GenericResponse;
+
+public class PaymentExtensionResponse extends GenericResponse {
+	
+	private boolean isPaymentExtension = false;
+
+	/**
+	 * @return the isPaymentExtension
+	 */
+	public boolean isPaymentExtension() {
+		return isPaymentExtension;
+	}
+
+	/**
+	 * @param isPaymentExtension the isPaymentExtension to set
+	 */
+	public void setPaymentExtension(boolean isPaymentExtension) {
+		this.isPaymentExtension = isPaymentExtension;
+	}
+	
+	
+	
+
+}
