296480b5f0251dbcdf63213b3d5488e4deed5d6a
Arumugam Thirugnanam <AThirug1@retail.nrgenergy.com>
2020-04-22 09:23:19 -0500

    Reliant Sales - Field Validation is changed.

--- src/main/java/com/multibrand/dto/request/SalesCreditReCheckRequest.java
+++ src/main/java/com/multibrand/dto/request/SalesCreditReCheckRequest.java
@@ -1,12 +1,9 @@
 package com.multibrand.dto.request;
 
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotEmpty;
 
-import com.multibrand.request.validation.BasicConstraint;
-import com.multibrand.request.validation.FormatConstraint;
+import com.multibrand.request.validation.NotEmpty;
 import com.multibrand.request.validation.SizeConstraint;
-import com.multibrand.request.validation.ValidDateTime;
 
 public class SalesCreditReCheckRequest extends SalesCreditCheckRequest {
 
