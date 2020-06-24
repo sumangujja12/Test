c31f59d68d875ed7c2e2a1d0bd584e49423931db
Perera, Nanayakkarage G Asha S <Asha.Perera@nrg.com>
2020-06-24 14:28:04 -0500

    Added "lastBillDate" to "getGMDStatementData" API.

--- src/main/java/com/multibrand/service/GMDService.java
+++ src/main/java/com/multibrand/service/GMDService.java
@@ -109,8 +109,8 @@ public class GMDService extends BaseAbstractService {
           
           ZetGmdInvdate zetGmdInvdate = new ZetGmdInvdate();
           ZesGmdStmt zesGmdStmt = new ZesGmdStmt();
-          ZetGmdStmt zetGmdStmt = new ZetGmdStmt();        
-
+          ZetGmdStmt zetGmdStmt = new ZetGmdStmt();  
+          
          Holder<ZetGmdInvdate>  holderZetGmdInvdate = new Holder<>();
          holderZetGmdInvdate.value = zetGmdInvdate;
          
@@ -125,11 +125,14 @@ public class GMDService extends BaseAbstractService {
          Holder<ZetGmdStmt>  holderZetGmdStmt = new Holder<>();
          holderZetGmdStmt.value = zetGmdStmt;
 		
+         Holder<String> holderLastBillDate = new Holder<String>();
+       
+        		 
 		try{
 			
-			stub.zeIsuGetGmdStmt(companyCode, accountNumber, esiId, month, year, holderAvgPrice, holderZetGmdInvdate, holderZettGmdRetchr,  holderZesGmdStmt, holderZetGmdStmt);
+			stub.zeIsuGetGmdStmt(companyCode, accountNumber, esiId, month, year, holderAvgPrice, holderZetGmdInvdate, holderLastBillDate,holderZettGmdRetchr,  holderZesGmdStmt, holderZetGmdStmt);
 						
-			gmdStatementBreakDownResp = handleGMDStatementResponse(holderZesGmdStmt, holderAvgPrice, holderZettGmdRetchr);
+			gmdStatementBreakDownResp = handleGMDStatementResponse(holderZesGmdStmt, holderAvgPrice, holderZettGmdRetchr,holderLastBillDate);
 			
 		}catch(Exception ex){
 			utilityloggerHelper.logTransaction("getGMDStatementDetails", false, request,ex, "", CommonUtil.getElapsedTime(startTime), "", sessionId, companyCode);
@@ -323,7 +326,7 @@ public class GMDService extends BaseAbstractService {
 		return current;
 	}
 	
-	private GMDStatementBreakDownResponse handleGMDStatementResponse(Holder<ZesGmdStmt>  holderZesGmdStmt,  Holder<BigDecimal> holderAvgPrice ,Holder<ZettGmdRetchr> holderZettGmdRetchr) {
+	private GMDStatementBreakDownResponse handleGMDStatementResponse(Holder<ZesGmdStmt>  holderZesGmdStmt,  Holder<BigDecimal> holderAvgPrice ,Holder<ZettGmdRetchr> holderZettGmdRetchr, Holder<String>holderLastBillDate) {
 
 		GMDStatementBreakDownResponse response = new GMDStatementBreakDownResponse();
 
@@ -365,6 +368,7 @@ public class GMDService extends BaseAbstractService {
 		response.setBreakdown(breakdown);
 		response.setReturnCharge(gmdReturnChargeList);		
 		response.setTotalUsage(zesGmdStmt.getCusage());
+		response.setLastBillDate(holderLastBillDate !=null ? holderLastBillDate.value : null);
 		return response;
 
 	}
