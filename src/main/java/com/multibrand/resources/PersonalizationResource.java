c0c2e2e18d4c389c60d774d4ccfe38eaf8deaba2
Venkat Anagani <Venkat.Anagani@nrg.com>
2020-09-28 14:32:07 -0500

    OAM - personalization service changes

--- src/main/java/com/multibrand/resources/PersonalizationResource.java
+++ src/main/java/com/multibrand/resources/PersonalizationResource.java
@@ -1,8 +1,8 @@
 package com.multibrand.resources;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,6 +13,7 @@ import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.multibrand.dto.request.PersonalizationRequest;
 import com.multibrand.dto.response.TCSPersonalizedFlagsDTO;
 import com.multibrand.service.PersonalizationService;
 import com.multibrand.util.Constants;
@@ -42,13 +43,13 @@ public class PersonalizationResource implements Constants {
 	@Path("getPersonalizedFlags")
 	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,})
-	public Response getPersonalizedFlags(@FormParam("bp") String bp,@FormParam("ca") String ca){
+	public Response getPersonalizedFlags(@Valid PersonalizationRequest request){
 		
 		Response response = null;
 
 		TCSPersonalizedFlagsResponse tcsPersonalizedFlagsResponse = new TCSPersonalizedFlagsResponse();
 		
-		TCSPersonalizedFlagsDTO tcsPersonalizedFlagsDTO = personalizationService.getPersonalizedFlags(bp,ca);
+		TCSPersonalizedFlagsDTO tcsPersonalizedFlagsDTO = personalizationService.getPersonalizedFlags(request.getBp(),request.getCa());
 		
 		tcsPersonalizedFlagsResponse.setTcsPersonalizedFlagsDTO(tcsPersonalizedFlagsDTO);
 
