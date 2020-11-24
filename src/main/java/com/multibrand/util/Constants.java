ce61baa8c2fda1d9a6cdd5a2bf5a8b5b56eff779
Srini Kodali <skodali1@nrg.com>
2020-11-24 02:09:29 +0000

    Updated Constants.java

--- src/main/java/com/multibrand/util/Constants.java
+++ src/main/java/com/multibrand/util/Constants.java
@@ -1654,5 +1654,6 @@ public interface Constants {
 
 	public static final String API_OFFER_DETAILS = "offer-details";
 	public static final String CHANNEL_WEB_CLOUD = "WEBCLOUD";
+	public static final String VALIDATE_POSID_WITH_KBA_REST = "validatePOSIdwithKBA";
 	
 }
