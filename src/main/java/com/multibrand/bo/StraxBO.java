49e10e7625cff8ef8051f71b738c88dbb9a38db3
VRajendran <VRajendran@TXHQVRAJEN-L1.nrgenergy.com>
2021-03-31 16:47:14 -0500

    Fixed code smell

--- src/main/java/com/multibrand/bo/StraxBO.java
+++ src/main/java/com/multibrand/bo/StraxBO.java
@@ -2,7 +2,6 @@ package com.multibrand.bo;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
