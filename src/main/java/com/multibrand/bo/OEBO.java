ea8d3917ded7ac5a7e3cf917a201f17590483277
AThirug1 <AThirug1@TXHQATHIRUGN-L1.nrgenergy.com>
2021-04-07 17:43:06 -0500

    Reliant Sales - Internal Server Issue Fixed

--- src/main/java/com/multibrand/bo/OEBO.java
+++ src/main/java/com/multibrand/bo/OEBO.java
@@ -5704,7 +5704,7 @@ private GetKBAQuestionsResponse createKBAQuestionResposne(KbaQuestionResponse kb
 					tokenResponse = new TokenizedResponse();
 				}
 				
-				if (request.getNoid().equalsIgnoreCase("TRUE")|| tokenResponse.getResultCode().equals(Constants.RESULT_CODE_SUCCESS)
+				if (StringUtils.equalsIgnoreCase(request.getNoid(), FLAG_TRUE)|| tokenResponse.getResultCode().equals(Constants.RESULT_CODE_SUCCESS)
 				&& StringUtils.isNotBlank(tokenResponse.getReturnToken())) {
 					
 					if(null != tokenResponse){
@@ -5712,7 +5712,7 @@ private GetKBAQuestionsResponse createKBAQuestionResposne(KbaQuestionResponse kb
 							+ request.getAffiliateId()
 							+ ":: got token back."+tokenResponse.getReturnToken());
 					}
-					if (request.getNoid().equalsIgnoreCase("TRUE") 
+					if (StringUtils.equalsIgnoreCase(request.getNoid(), FLAG_TRUE)
 							||!CommonUtil.checkTokenDown(tokenResponse.getReturnToken())) {
 						
 						if(StringUtils.isNotEmpty(request.getProspectId())) {
