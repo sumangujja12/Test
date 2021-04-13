1392cd9b07c532cb0422828b81fd6e202cca2706
VRajendran <VRajendran@TXHQVRAJEN-L1.nrgenergy.com>
2021-04-13 10:59:51 -0500

    Updated strax invoice response

--- src/main/java/com/multibrand/vo/response/StraxInvoiceResponse.java
+++ src/main/java/com/multibrand/vo/response/StraxInvoiceResponse.java
@@ -2,16 +2,7 @@ package com.multibrand.vo.response;
 
 public class StraxInvoiceResponse extends GenericResponse{
 	
-	private String xCode;
-
-	public String getxCode() {
-		return xCode;
-	}
-
-	public void setxCode(String xCode) {
-		this.xCode = xCode;
-	}
-
+	
 	
 
 	
