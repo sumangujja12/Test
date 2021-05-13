333deb3658cfbebcd66c41da56be14d1b807d26b
Arumugam Thirugnanam <AThirug1@reliant.com>
2021-05-13 15:55:52 -0500

    Reliant Sales - Validation annotation added for Post methods

--- src/main/java/com/multibrand/resources/SalesAPIResource.java
+++ src/main/java/com/multibrand/resources/SalesAPIResource.java
@@ -139,7 +139,7 @@ public class SalesAPIResource extends BaseResource {
 	@Path(API_AVAILABLE_DATES)
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
 	@Produces({ MediaType.APPLICATION_JSON })
-	public Response getESIDAndCalendarDates(SalesEsidCalendarRequest request) {
+	public Response getESIDAndCalendarDates(@Valid SalesEsidCalendarRequest request) {
 		long startTime = CommonUtil.getStartTime();
 		Response response = null;
 		try{
@@ -211,7 +211,7 @@ public class SalesAPIResource extends BaseResource {
 	@Path(API_SUBMIT_ENROLLMENT)
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
 	@Produces({ MediaType.APPLICATION_JSON })
-	public Response submitEnrollment(SalesEnrollmentRequest request)
+	public Response submitEnrollment(@Valid SalesEnrollmentRequest request)
 			throws OEException {
 		long startTime = CommonUtil.getStartTime();
 		Response response = null;
@@ -233,7 +233,7 @@ public class SalesAPIResource extends BaseResource {
     @Path(API_GET_KBA_QUESTIONS)
     @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
     @Produces({ MediaType.APPLICATION_JSON })
-    public Response getKBAQuestions(GetKBAQuestionsRequest request) {
+    public Response getKBAQuestions(@Valid GetKBAQuestionsRequest request) {
 		long startTime = CommonUtil.getStartTime();
 		Response response=null;
        try{
@@ -252,7 +252,7 @@ public class SalesAPIResource extends BaseResource {
 	@Path(API_KBA_RESULT)
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
 	@Produces({ MediaType.APPLICATION_JSON })
-	public Response submitKBAAnswers(KbaAnswerRequest request) throws Exception {
+	public Response submitKBAAnswers(@Valid KbaAnswerRequest request) throws Exception {
 		long startTime = CommonUtil.getStartTime();
 		Response response=null;
 		
@@ -315,7 +315,7 @@ public class SalesAPIResource extends BaseResource {
 	@Path(API_ESID)
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
     @Produces({ MediaType.APPLICATION_JSON })
-	public Response getESIDDetails(EsidRequest request){
+	public Response getESIDDetails(@Valid EsidRequest request){
 		Response response = null;
 		long startTime = CommonUtil.getStartTime();
 		try{
@@ -334,7 +334,7 @@ public class SalesAPIResource extends BaseResource {
 	@Path(API_ESID_RESIDENTIAL)
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
     @Produces({ MediaType.APPLICATION_JSON })
-	public Response getESIDResidentialDetails(EsidRequest request){
+	public Response getESIDResidentialDetails(@Valid EsidRequest request){
 		Response response = null;
 		long startTime = CommonUtil.getStartTime();
 		try{
@@ -353,7 +353,7 @@ public class SalesAPIResource extends BaseResource {
     @Path(KBA_OE)
     @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED})
     @Produces({ MediaType.APPLICATION_JSON })
-    public Response getKBAQuestionsWithinOE(GetOEKBAQuestionsRequest request){
+    public Response getKBAQuestionsWithinOE(@Valid GetOEKBAQuestionsRequest request){
 		long startTime = CommonUtil.getStartTime();
 		Response response=null;
        try{
@@ -438,7 +438,7 @@ public class SalesAPIResource extends BaseResource {
 	@Path(API_TDSP)
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
 	@Produces({ MediaType.APPLICATION_JSON })
-	public Response getTDSP(SalesTDSPRequest request) {
+	public Response getTDSP(@Valid SalesTDSPRequest request) {
 		logger.info("SalesTDSPRequest : {}", request);
 		Response response = null;
 		try{
@@ -459,7 +459,7 @@ public class SalesAPIResource extends BaseResource {
 	@Path(SALES_API_ESID_VALIDATION)
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
 	@Produces({ MediaType.APPLICATION_JSON })
-	public Response validateEsid(ValidateEsidRequest validateEsidRequest) {
+	public Response validateEsid(@Valid ValidateEsidRequest validateEsidRequest) {
 		logger.info("In validateEsid");
 		Response response = null;
 			
