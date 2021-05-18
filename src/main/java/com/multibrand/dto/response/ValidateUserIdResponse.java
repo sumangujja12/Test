a5a4282d19cfbc272393c542f77228856c0ae2f3
Kalyani <Kalyani@10.99.80.79>
2021-05-18 19:33:49 +0530

    OE- Validate UserId and CA

--- src/main/java/com/multibrand/dto/response/ValidateUserIdResponse.java
+++ src/main/java/com/multibrand/dto/response/ValidateUserIdResponse.java
@@ -1,6 +1,9 @@
+
 package com.multibrand.dto.response;
 
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name="ValidateUserIdResponse")
 public class ValidateUserIdResponse extends SalesBaseResponse {
 	
 }
