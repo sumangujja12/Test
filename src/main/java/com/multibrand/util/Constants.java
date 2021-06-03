494f8ea818cfd377818a96f5ede9845b81e0f689
VRajendran <VRajendran@TXHQVRAJEN-L1.nrgenergy.com>
2021-06-03 08:43:20 -0500

    Updated request fields for Zirtue payment

--- src/main/java/com/multibrand/util/Constants.java
+++ src/main/java/com/multibrand/util/Constants.java
@@ -1668,10 +1668,7 @@ public interface Constants {
 	public static final String GIACT_ERROR_02="GIACT_ERROR_02";
 	public static final String GIACT_ERROR_03="GIACT_ERROR_03";
 	public static final String BANK_AUTOPAY_INVALID_BANK_ACCOUNT = "MSG_CCSERR_2_INVALID_BNK";
-	
-
-	public static final String ZIRTUE_RECEIPT_NUMBER = "ZIRTUE.REL.0102.1";
-	
+		
 	public static final String FLAG_OTHER = "Other";
 
 }
